c7cdffde2573a2ed6b19083e16d51fa6c780043d
Iffern <kicajka1999@gmail.com>
2021-03-20 16:47:34 +0100

    Initial commit - Spring setup

---
+++ src/main/java/com/gupb/manager/GUPBManager/GupbManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gupb.manager.GUPBManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GupbManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GupbManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gupb/manager/GUPBManager/GupbManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gupb.manager.GUPBManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GupbManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
