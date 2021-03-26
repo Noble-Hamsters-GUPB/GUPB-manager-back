17e2337ba287e810d00a23253d944e8708ab1e78
Tequilac <58514571+tequilac@users.noreply.github.com>
2021-03-26 12:20:50 +0100

    Fix students table not creating due to using wrong table name

--- src/main/java/com/gupb/manager/model/Student.java
+++ src/main/java/com/gupb/manager/model/Student.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = Team.TABLE_NAME)
+@Table(name = Student.TABLE_NAME)
 public class Student {
 
     public static final String TABLE_NAME = "student";
