b302ecc7676f562eeb5846a45230b6d7f005886b
Kamil Koczera <Koczera9@wp.pl>
2021-05-09 21:52:41 +0200

    Read teams from database

--- src/main/java/com/gupb/manager/repositories/TeamRepository.java
+++ src/main/java/com/gupb/manager/repositories/TeamRepository.java
@@ -1,12 +1,17 @@
 package com.gupb.manager.repositories;
 
+import com.gupb.manager.model.Student;
 import com.gupb.manager.model.Team;
 import com.gupb.manager.model.Tournament;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface TeamRepository extends CrudRepository<Team, Integer> {
+    @Override
+    @Query("select team from Team team join fetch team.tournament tournament")
+    List<Team> findAll();
 
     List<Team> findByTournament(Tournament tournament);
 }
