d66a274b6dab75088fc0251645eddc50a79a9761
Tequilac <58514571+tequilac@users.noreply.github.com>
2021-05-10 20:54:27 +0200

    Create round controller, improve scheduler, add queries to repositories

--- src/main/java/com/gupb/manager/repositories/RequirementRepository.java
+++ src/main/java/com/gupb/manager/repositories/RequirementRepository.java
@@ -1,7 +1,15 @@
 package com.gupb.manager.repositories;
 
 import com.gupb.manager.model.Requirement;
+import com.gupb.manager.model.Round;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RequirementRepository extends CrudRepository<Requirement, Integer> {
+
+    @Override
+    @Query("select r from Requirement r join fetch r.tournament t")
+    List<Requirement> findAll();
 }
