04df0ed9146497211364ce4ccbb797ce9ae9b330
Helena Obertynska <obertynska@student.agh.edu.pl>
2021-05-11 17:06:32 +0200

    add requirementController second version (without status verification)

--- src/main/java/com/gupb/manager/repositories/RequirementRepository.java
+++ src/main/java/com/gupb/manager/repositories/RequirementRepository.java
@@ -1,6 +1,7 @@
 package com.gupb.manager.repositories;
 
 import com.gupb.manager.model.Requirement;
+import com.gupb.manager.model.Tournament;
 import com.gupb.manager.model.Round;
 import org.springframework.data.jpa.repository.Query;
 import com.gupb.manager.model.Tournament;
@@ -9,6 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RequirementRepository extends CrudRepository<Requirement, Integer> {
+    List<Requirement> findByTournament(Tournament tournament);
 
     @Override
     @Query("select r from Requirement r join fetch r.tournament t")
