67b38e3c617133ad6461bd3b1ec74e1f671af670
Helena Obertynska <obertynska@student.agh.edu.pl>
2021-05-11 19:02:55 +0200

    add without status verification

--- src/main/java/com/gupb/manager/providers/RequirementProvider.java
+++ src/main/java/com/gupb/manager/providers/RequirementProvider.java
@@ -1,12 +1,14 @@
-package com.gupb.manager.controllers;
+package com.gupb.manager.providers;
 
 import com.gupb.manager.model.Requirement;
+import com.gupb.manager.model.RequirementStatus;
 import com.gupb.manager.model.Tournament;
 import com.gupb.manager.providers.GameProvider;
 import com.gupb.manager.repositories.RequirementRepository;
 import com.gupb.manager.repositories.TeamRepository;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +25,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
-@RestController
-@RequestMapping("/api/v1/")
-public class RequirementController {
+@Component
+public class RequirementProvider {
 
     @Autowired
     private RequirementRepository requirementRepository;
@@ -35,7 +35,9 @@ public class RequirementController {
         List<Requirement> requirements = requirementRepository.findByTournament(tournament);
         StringBuilder stringBuilder = new StringBuilder();
         for (Requirement requirement : requirements) {
-            stringBuilder.append(requirement.getPackageInfo()).append("\n");
+            if (requirement.getStatus().equals(RequirementStatus.APPROVED)) {
+                stringBuilder.append(requirement.getPackageInfo()).append("\n");
+            }
         }
         try {
             Files.write(Paths.get(path), stringBuilder.toString().getBytes(), StandardOpenOption.APPEND);
